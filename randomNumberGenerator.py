9e851ca5ee822abc68278a5cfbc5a64b0102d8d7
Right2Drive <bjpfannm@ucalgary.ca>
2016-04-04 16:46:29 -0600

    Refined the script

--- randomNumberGenerator.py
+++ randomNumberGenerator.py
@@ -1,6 +1,24 @@
 from random import randint
 
-minimum = input("Enter the minimum amount: ")
-maximum = input("Enter the maximum amount: ")
+flag = 1
+maximum = 0
+while flag:
+    maximum = int(input("Enter the maximum amount: "))
+    if (maximum < 20):
+        print("Not large enough   ;)   ---> please try again")
+        flag = 1
+    else:
+        flag = 0
 
-print(randint(minimum,maximum))
+bug_list = []
+
+for index in range(0, 20):
+    flag = 0
+    while flag == 0:
+        bugNum = randint(0, maximum)
+        if bugNum in bug_list:
+            flag = 0
+        else:
+            bug_list.append(bugNum)
+            flag = 1
+            print("Index " + str(index) + ": " + str(bugNum))
